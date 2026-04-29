d10faf30dc1b1d634ba2c1c53be84929cef27e3e
vulcan-sweck <ibarnor076@gmail.com>
2026-04-29 02:04:08 +0000

    Added the stl, schematic and audio in the file folder

--- include/Definitions.h
+++ include/Definitions.h
@@ -8,11 +8,10 @@
 #define RFID_RST_PIN 9
 
 
-#define MP3_RX 5
-#define MP3_TX 6
+#define MP3_RX 6
+#define MP3_TX 5
 
 #define DOOR_PIN 3
-#define BUZZER_PIN 4
 
 
 /* ---------------- RFID CARD STORAGE ---------------- */
--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,13 @@
 MP3Player player(MP3_RX, MP3_TX);
 RFIDReader rfid(RFID_SS_PIN, RFID_RST_PIN);
 
-
+//1 is for access granted
+//2 is for access denied
+//3 is for welcome message
 void setup()
 {
     Serial.begin(9600);
     pinMode(DOOR_PIN, OUTPUT);
-    pinMode(BUZZER_PIN, OUTPUT);
 
     rfid.begin();
     if (!player.begin())
@@ -20,13 +21,11 @@ void setup()
         Serial.println("DFPlayer not detected!");
         while (true);
     }
-
-    player.setVolume(30);
-    player.start(1);   // plays 0001.mp3
 }
 
 void loop()
 {
+
     if (rfid.readCard(scannedUID))
     {
         rfid.printCardID(scannedUID);
@@ -34,18 +33,22 @@ void loop()
         if (rfid.isAllowed(scannedUID))
         {
             Serial.println("Access Granted");
-
+            player.setVolume(30);
+            player.start(1);
+            delay(2000);
+            player.start(3);
+            
             digitalWrite(DOOR_PIN, HIGH);
+            Serial.println("Door Opened");
             delay(3000);
             digitalWrite(DOOR_PIN, LOW);
+            Serial.println("Door Closed");
         }
         else
         {
             Serial.println("Access Denied");
-
-            digitalWrite(BUZZER_PIN, HIGH);
-            delay(500);
-            digitalWrite(BUZZER_PIN, LOW);
+            player.setVolume(30);
+            player.start(2);
         }
     }
 }
